00a144bd70d28aa1f4b0c64190764327ed6fb58b
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-07-30 10:41:58 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -7,27 +7,47 @@ TEST(SoundexTest, HandlesEmptyString) {
 
 TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex("B"), "B000");
 }
 
-TEST(SoundexTest, HandlesBasicFunctionality) {
-    EXPECT_EQ(generateSoundex("Smith"), "S530");
-    EXPECT_EQ(generateSoundex("Smyth"), "S530");
-}
-
-TEST(SoundexTest, HandlesDifferentStartingLetters) {
+TEST(SoundexTest, HandlesSimpleCase) {
     EXPECT_EQ(generateSoundex("Robert"), "R163");
-    EXPECT_EQ(generateSoundex("Rupert"), "R163");
 }
 
-TEST(SoundexTest, HandlesSimilarSounds) {
+TEST(SoundexTest, HandlesDuplicateCodes) {
     EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+}
+
+TEST(SoundexTest, HandlesVowelsAndIgnoredLetters) {
+    EXPECT_EQ(generateSoundex("Aeiouhwy"), "A000");
+}
+
+TEST(SoundexTest, HandlesVariousCases) {
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+    EXPECT_EQ(generateSoundex("Jackson"), "J250");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+}
+
+TEST(SoundexTest, HandlesSimilarSoundingNames) {
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+    EXPECT_EQ(generateSoundex("Rubin"), "R150");
+}
+
+TEST(SoundexTest, HandlesSameNumberSeparatedByVowel) {
+    EXPECT_EQ(generateSoundex("Moses"), "M020");
+    EXPECT_EQ(generateSoundex("Morris"), "M620");
+}
+
+TEST(SoundexTest, HandlesHAndWBetweenSameNumber) {
     EXPECT_EQ(generateSoundex("Ashcroft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymchwak"), "T522");
 }
 
-TEST(SoundexTest, HandlesDifferentLengths) {
+TEST(SoundexTest, HandlesNameWithNoConsonants) {
+    EXPECT_EQ(generateSoundex("AEIOUY"), "A000");
+}
+
+TEST(SoundexTest, PadsWithZerosIfNeeded) {
     EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex("Al"), "A400");
-    EXPECT_EQ(generateSoundex("Alii"), "A400");
+    EXPECT_EQ(generateSoundex("Ab"), "A100");
+    EXPECT_EQ(generateSoundex("Abc"), "A120");
 }
-
