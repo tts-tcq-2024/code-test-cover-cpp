1aad44fb16cf54604273df0c5abc15b57f9875ba
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-07-30 10:45:46 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,8 +1,11 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
-
+#ifndef SOUNDEX_HPP
+#define SOUNDEX_HPP
 #include <string>
-
+char getSoundexCode(char c);
+bool SoundexLengthCheck(const std::string& soundex);
+bool SoundexCodeCheck(char code, char prevCode);
+std::string IncrementSoundex(const std::string& soundex, const std::string& name, char prevCode);
 std::string generateSoundex(const std::string& name);
-
-#endif // SOUNDEX_H
+std::string padSoundex(const std::string& soundex);
+ 
+#endif 
